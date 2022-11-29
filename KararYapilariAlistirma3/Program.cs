ce17b11789e6d19b93b79b465e21f2a8ce72b7b6
Furkan Ertekin <furkanertekin0@gmail.com>
2022-11-29 17:18:36 +0300

    first commit

---
+++ KararYapilariAlistirma3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KararYapilariAlistirma3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kullanıcı Kodu : ");
+            string kulllaiciKodu = Console.ReadLine();
+
+            switch (kulllaiciKodu)
+            {
+                case "AA":
+                case "BB":
+                case "CC":
+                    Console.WriteLine("Admin yetkisine sahipsiniz");
+                    break;
+
+                case "DD":
+                    Console.WriteLine("Güçlü kullanıcı yetkisine sahipsiniz");
+                    break;
+
+                case "FF":
+                case "EE":
+                case "GG":
+                    Console.WriteLine("Standart kullanıcı yetkisine sahipsiniz");
+                    break;
+
+                default:
+                    Console.WriteLine("Hatalı kullanıcı kodu");
+                    break;
+
+            }
+
+            Console.WriteLine("İyi günler dileriz..");
+            Console.ReadLine();
+        }
+    }
+}
